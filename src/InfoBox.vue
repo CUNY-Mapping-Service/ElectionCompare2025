08644a7e13e9d89a923200eaaf613d681849269f
stevenromalewski <sromalewski@gc.cuny.edu>
2025-11-04 21:37:51 -0500

    66 pct

--- src/InfoBox.vue
+++ src/InfoBox.vue
@@ -22,7 +22,7 @@ interface Props {
     allFeatures?: any[]; // Array of all features for citywide calculations
 }
 
-const scannerPercent = 53
+const scannerPercent = 66
 const props = defineProps<Props>();
 const emit = defineEmits<{
     close: []
